bf7181ef74930f736e241f468cf1a810a8a0db87
Alex <Aronin.2001@gmail.com>
2021-11-29 19:42:01 +0400

    block tovars and Paginations are completed

--- src/js/script.js
+++ src/js/script.js
@@ -27,3 +27,102 @@ $('.baner').slick({
     slidesToShow: 1,
     adaptiveHeight: true
 });
+
+// tovars IMG slider
+// images slider tovars
+(function ($) {
+    $.fn.HvrSlider = function () {
+      return this.each(function () {
+        var el = $(this);
+        if (el.find('img').length > 1) {
+          var hvr = $('<div>', {
+            class: 'hvr',
+            append: [
+              $('<div>', {
+                class: 'hvr__images',
+                append: $('<div>', {
+                  class: 'hvr__sectors',
+                }),
+              }),
+              $('<div>', {
+                class: 'hvr__dots',
+              }),
+            ],
+            insertAfter: el,
+            prepend: el,
+          });
+          var hvrImages = $('.hvr__images', hvr);
+          var hvrImage = $('img', hvr);
+          var hvrSectors = $('.hvr__sectors', hvr);
+          var hvrDots = $('.hvr__dots', hvr);
+          el.prependTo(hvrImages);
+          hvrImage.each(function () {
+            hvrSectors.prepend('<div class="hvr__sector"></div>');
+            hvrDots.append('<div class="hvr__dot"></div>');
+          });
+          $('.hvr__dot:first', hvrDots).addClass('hvr__dot--active');
+          var setActiveEl = function (el) {
+            hvrImage.hide().eq(el.index()).show();
+            $('.hvr__dot', hvrDots).removeClass('hvr__dot--active').eq(el.index()).addClass('hvr__dot--active');
+          };
+          $('.hvr__sector', hvrSectors).hover(function () {
+            setActiveEl($(this));
+          });
+          hvrSectors.on('touchmove', function (e) {
+            e.preventDefault();
+            var position = e.originalEvent.changedTouches[0];
+            var target = document.elementFromPoint(position.clientX, position.clientY);
+            if ($(target).is('.hvr__sector')) {
+              setActiveEl($(target));
+            }
+          });
+        }
+      });
+    };
+})(jQuery);
+
+$('.images').HvrSlider();
+
+// tovars slick
+$('.tovars__items').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+// change button slick in tovars
+const btns = document.querySelectorAll('.tovars .slick-next, .tovars .slick-prev');
+
+btns.forEach(function(item) {
+  item.innerHTML = "<i class='icon-arrow-right'></i>";
+});
